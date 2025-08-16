6f79ea7da8e30a54c7ac666850d3054595107a3c
tikature <detikaa10@gmail.com>
2025-08-17 01:08:27 +0800

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"my-recipe/internal/data"
 	"my-recipe/internal/handlers"
@@ -11,13 +12,19 @@ import (
 func main() {
 	log.Printf("Starting server with %d recipes loaded", len(data.ResepData))
 
-	// static
+	// Static files
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	// routes
+	// Routes
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/resep/", handlers.DetailHandler)
 
-	log.Println("Server berjalan di http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// PORT dari environment
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // lokal
+	}
+
+	log.Printf("Server berjalan di http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
